e58f10b6b4ea673fa873c7addacafda331ff24c4
Embedded Electronics <embeddedelectronics.in@gmail.com>
2026-03-15 11:22:32 +0530

    Create 01_LED-Blinking-Using-Arduino.ino

---
+++ 01_LED-Blinking-Using-Arduino.ino
@@ -0,0 +1,15 @@
+int ledPin = 13;   // LED connected to digital pin 13
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);   // Set pin 13 as OUTPUT
+}
+
+void loop()
+{
+  digitalWrite(ledPin, HIGH);   // Turn LED ON
+  delay(1000);                  // Wait for 1 second
+
+  digitalWrite(ledPin, LOW);    // Turn LED OFF
+  delay(1000);                  // Wait for 1 second
+}
